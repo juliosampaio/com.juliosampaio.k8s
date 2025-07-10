01aaa346642063a9687f717e695adb1583a7a349
Julio Sampaio <julio.sampaio@emma-sleep.com>
2025-07-10 20:04:18 +0100

    Fix: populate hosts/common.nix with htop package

---
+++ hosts/common.nix
@@ -0,0 +1,5 @@
+{ pkgs }:
+
+[
+  pkgs.htop
+]
--- hosts/k8s-cluster-node-1.nix
+++ hosts/k8s-cluster-node-1.nix
@@ -1,5 +1,11 @@
 { pkgs, common }:
 
+let
+  dinosay = pkgs.writeShellScriptBin "dinosay" ''
+    exec ${pkgs.cowsay}/bin/cowsay -f trex "$@"
+  '';
+in
+
 common ++ [
-  pkgs.dinosay
+  dinosay
 ]
